c155dbb223c8dfb09549d34153cbc6ff9e04b1a0
thefosk <marco@mashape.com>
2012-08-16 18:48:37 -0700

    2.0

--- test.rb
+++ test.rb
@@ -6,23 +6,23 @@ class Test
   def initialize(public_key, private_key, consumer_key, consumer_secret, redirect_url)
     @authentication_handlers = Array.new
     @authentication_handlers << Mashape::MashapeAuthentication.new(public_key, private_key)
-    @authentication_handlers << Mashape::OAuthAuthentication.new(consumer_key, consumer_secret, redirect_url)
+    # @authentication_handlers << Mashape::OAuthAuthentication.new(consumer_key, consumer_secret, redirect_url)
   end
 
-  def authenticate_oauth(access_token, access_secret = nil)
-     @authentication_handlers.each do |handler|
-       if handler.kind_of? Mashape::OAuthAuthentication
-         handler.handleParams[:access_token] = access_token
-         handler.handleParams[:access_secret] = access_secret
-         break
-       end
-     end
-     return self
-   end
+#  def authenticate_oauth(access_token, access_secret = nil)
+#     @authentication_handlers.each do |handler|
+#       if handler.kind_of? Mashape::OAuthAuthentication
+#         handler.handleParams[:access_token] = access_token
+#         handler.handleParams[:access_secret] = access_secret
+#         break
+#       end
+#     end
+#     return self
+#   end
    
-   def get_oauth_url
-     return "https://" + PUBLIC_DNS + "/oauth"
-   end
+#   def get_oauth_url
+#     return "https://" + PUBLIC_DNS + "/oauth"
+#   end
 
   def getHello(name, &callback)
     parameters = {"_method" => "getHello", "name" => name}
@@ -36,13 +36,9 @@ class Test
   
 end
 
-test = Test.new("PUBVV-sMYgCVmTBI31iwEw9Qz8_XvyQk", "PRIpOYUeeFF3sV%KGsSSxCINDLBpmXoH", "consumer-key", "consumer-secret", "http://mashape.com/validate");
+test = Test.new("public", "private", "consumer-key", "consumer-secret", "http://mashape.com/validate");
 
-#test.authenticate_oauth("access-token", "access-secret");
-
-#puts test.getHello("Marco").body
-
-test.authenticate_oauth("access-token")
+#test.authenticate_oauth("access-token")
 
 puts test.getHello("Marco").body
 puts test.getHello("Marco").body
