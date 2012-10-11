88370aceda3853ab2da29003512b5fc96b7296cb
thefosk <marco@mashape.com>
2012-10-11 15:21:56 -0700

    refactor

--- lib/authentication/oauth2_authentication.rb
+++ lib/authentication/oauth2_authentication.rb
@@ -2,8 +2,8 @@ require File.join(File.dirname(__FILE__), "/oauth_authentication.rb")
 
 module Mashape
   class OAuth2Authentication < OAuthAuthentication
-     def initialize(consumer_key, consumer_secret, redirect_url)
-       super(consumer_key, consumer_secret, redirect_url)
+     def initialize(consumer_key, consumer_secret, callback_url)
+       super(consumer_key, consumer_secret, callback_url)
      end
   end
 end
--- lib/authentication/oauth_authentication.rb
+++ lib/authentication/oauth_authentication.rb
@@ -3,11 +3,11 @@ require File.join(File.dirname(__FILE__), "/authentication.rb")
 module Mashape
   class OAuthAuthentication < Authentication
     
-    def initialize(consumer_key, consumer_secret, redirect_url)
+    def initialize(consumer_key, consumer_secret, callback_url)
       super()
       @params[:consumer_key] = consumer_key
       @params[:consumer_secret] = consumer_secret
-      @params[:redirect_url] = redirect_url
+      @params[:callback_url] = callback_url
     end
   end
 end
