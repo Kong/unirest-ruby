7e3ee05a2aa5a00c40cbd64506fd3da20f1f46b0
Max Lielje <max@maxlielje.co>
2014-10-07 22:56:19 +0200

    update tests to make them pass

--- test/unirest_test.rb
+++ test/unirest_test.rb
@@ -55,11 +55,11 @@ module Unirest
 
     should "POST custom body" do
       
-      response = Unirest.post("http://httpbin.org/post", parameters:"hello")
+      response = Unirest.post("http://httpbin.org/post", headers:{ "Content-Type" => "application/json" }, parameters:{'name' => 'Mark'}.to_json)
       assert response.code == 200
 
       data = response.body['data']
-      assert data == "hello"
+      assert data == '{"name":"Mark"}'
 
     end
 
@@ -92,8 +92,9 @@ module Unirest
       response = Unirest.delete("http://httpbin.org/delete", parameters:{'name' => 'Mark', 'nick'=> "sinz s"})
       assert response.code == 200
 
-      data = response.body['data']
-      assert data == "name=Mark&nick=sinz%20s"
+      data = response.body['form']
+      assert data['name'] == "Mark"
+      assert data['nick'] == "sinz s"
 
     end
 
