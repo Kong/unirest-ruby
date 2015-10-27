2797ee98def2737c56d4473a3af3eebc2a0b3d4e
Jonathan Ballard <jonathan.ballard@gmail.com>
2015-10-26 21:30:03 -0400

    - Refactor method - Add proxy setting

--- lib/unirest.rb
+++ lib/unirest.rb
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -20,7 +20,7 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# 
+#
 
 require 'rubygems'
 require 'rest-client'
@@ -29,54 +29,44 @@ require File.join(File.dirname(__FILE__), "/unirest/http_request.rb")
 require File.join(File.dirname(__FILE__), "/unirest/http_response.rb")
 
 module Unirest
-  
+
   @@timeout         = 10
   @@default_headers = {}
+  @@proxy           = nil
   @@user_agent      = "unirest-ruby/1.1"
-  
+
   class HttpClient
-  
-    def self.request(method, url, headers, body, auth, timeout, &callback)
+
+    def self.request(method, url, headers, body, auth, timeout, proxy, &callback)
       http_request = Unirest::HttpRequest.new(method, url, headers, body, auth)
-      
+
       if callback
         return Thread.new do
-          callback.call(self.internal_request(http_request, timeout))
+          callback.call(self.internal_request(http_request, timeout,proxy))
         end
       else
-        return self.internal_request(http_request, timeout)
+        return self.internal_request(http_request, timeout,proxy)
       end
     end
-    
-    def self.internal_request(http_request, timeout)      
+
+    def self.internal_request(http_request, timeout, proxy)
       # Set the user agent
       http_request.add_header("user-agent", Unirest.user_agent)
       http_request.add_header("accept-encoding", "gzip")
-      
+
       http_response = nil;
 
       begin
-        case http_request.method
-          when :get
-            http_response = RestClient::Request.execute(:method => :get, :url => http_request.url, :headers => http_request.headers, :timeout => timeout)
-          when :post
-             http_response = RestClient::Request.execute(:method => :post, :url => http_request.url, :payload => http_request.body, :headers => http_request.headers, :timeout => timeout)
-          when :put
-            http_response = RestClient::Request.execute(:method => :put, :url => http_request.url, :payload => http_request.body, :headers => http_request.headers, :timeout => timeout)
-          when :delete
-            http_response = RestClient::Request.execute(:method => :delete, :url => http_request.url, :payload => http_request.body, :headers => http_request.headers, :timeout => timeout)
-          when :patch
-            http_response = RestClient::Request.execute(:method => :patch, :url => http_request.url, :payload => http_request.body, :headers => http_request.headers, :timeout => timeout)
-        end
-     rescue RestClient::RequestTimeout
-      raise 'Request Timeout'
-     rescue RestClient::Exception => e
-         http_response = e.response
+        http_response = RestClient::Request.execute(:method => http_request.method.to_sym, :url => http_request.url, :headers => http_request.headers, :timeout => timeout, payload: (http_request.method != :get) ? http_request.body : nil)
+      rescue RestClient::RequestTimeout
+        raise 'Request Timeout'
+      rescue RestClient::Exception => e
+        http_response = e.response
       end
 
       return Unirest::HttpResponse.new(http_response)
     end
-    
+
   end
 
   def self.default_header(name, value)
@@ -91,6 +81,10 @@ module Unirest
     @@timeout = seconds
   end
 
+  def self.proxy(url)
+    @@proxy = url
+  end
+
   def self.user_agent(user_agent=@@user_agent)
     if user_agent
       @@user_agent = user_agent
@@ -100,23 +94,23 @@ module Unirest
   end
 
   def self.get(url, headers: {}, parameters: nil, auth:nil, &callback)
-    return HttpClient.request(:get, url, headers.merge(@@default_headers), parameters, auth, @@timeout, &callback)
+    return HttpClient.request(:get, url, headers.merge(@@default_headers), parameters, auth, @@timeout, @@proxy, &callback)
   end
-    
+
   def self.post(url, headers: {}, parameters: nil, auth:nil, &callback)
-    return HttpClient.request(:post, url, headers.merge(@@default_headers), parameters, auth, @@timeout, &callback)
+    return HttpClient.request(:post, url, headers.merge(@@default_headers), parameters, auth, @@timeout, @@proxy, &callback)
   end
-    
+
   def self.delete(url, headers: {}, parameters: nil, auth:nil, &callback)
-    return HttpClient.request(:delete, url, headers.merge(@@default_headers), parameters, auth, @@timeout, &callback)
+    return HttpClient.request(:delete, url, headers.merge(@@default_headers), parameters, auth, @@timeout, @@proxy, &callback)
   end
-    
+
   def self.put(url, headers: {}, parameters: nil, auth:nil, &callback)
-    return HttpClient.request(:put, url, headers.merge(@@default_headers), parameters, auth, @@timeout, &callback)
+    return HttpClient.request(:put, url, headers.merge(@@default_headers), parameters, auth, @@timeout, @@proxy, &callback)
   end
-    
+
   def self.patch(url, headers: {}, parameters: nil, auth:nil, &callback)
-    return HttpClient.request(:patch, url, headers.merge(@@default_headers), parameters, auth, @@timeout, &callback)
+    return HttpClient.request(:patch, url, headers.merge(@@default_headers), parameters, auth, @@timeout, @@proxy, &callback)
   end
-  
+
 end
